473ef4b76f793fb84c9bebb50fb43491b4621898
Anna Ojdana <anna.ojdana@gmail.com>
2022-08-07 13:20:42 +0200

    styles for transactions

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,25 +1,27 @@
 import PropTypes from 'prop-types';
-// import styles from './TransactionHistory.module.css';
+import styles from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
+  const { transactions, table, table__header, table__heading, table__body, table__cell, table__row } =
+    styles;
   return (
-    <section className="transactions">
+    <section className={transactions}>
       {items.length > 0 && (
-        <table className="transaction-history">
-          <thead>
+        <table className={table}>
+          <thead className={table__header}>
             <tr>
-              <th>Type</th>
-              <th>Amount</th>
-              <th>Currency</th>
+              <th className={table__heading}>Type</th>
+              <th className={table__heading}>Amount</th>
+              <th className={table__heading}>Currency</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className={table__body}>
             {items.map(({ id, type, amount, currency }) => {
               return (
-                <tr key={id}>
-                  <td>{type}</td>
-                  <td>{amount}</td>
-                  <td>{currency}</td>
+                <tr key={id} className={table__row}>
+                  <td className={table__cell}>{type}</td>
+                  <td className={table__cell}>{amount}</td>
+                  <td className={table__cell}>{currency}</td>
                 </tr>
               );
             })}
